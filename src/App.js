0d10e4d1516130e958ee85628c6d3426059ea2fa
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-01-30 01:39:00 +0530

    Checkpoint-1: Pure React

---
+++ src/App.js
@@ -0,0 +1,33 @@
+const Pet = ({ name, animal, breed }) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {key: 1}, name),
+        React.createElement("h2", {key: 2}, animal),
+        React.createElement("h2", {key: 3}, breed)
+    ]);
+};
+
+const App = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {key: 1}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese",
+            key: 2
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel",
+            key: 3
+        }),
+        React.createElement(Pet, {
+            name: "Doink",
+            animal: "Cat",
+            breed: "Mixed",
+            key: 4
+        })
+    ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
