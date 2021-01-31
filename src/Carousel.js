df2b4c769fdcb22f2d152c8663e6a93e28d88328
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-01-31 22:34:51 +0530

    add Details route, add custom Babel config, add Carousel

---
+++ src/Carousel.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+      active: 0,
+    };
+  }
+  // add event listener
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["https://placecorgi.com/600/600"];
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+    return { photos };
+  }
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,49 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = (props) => {
-    return ( 
-        <h1>{props.id}</h1>
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+  componentDidMount() {
+    // It's a lifecycle method
+    // Similar to useEffect Hook, but it doesn't take dependencies like useEffect does.
+    // This runs exactly once when it's first startup, then it doesn't run
+    // It's useful for doing AJAX requests.
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        });
+      })
+      .catch((err) => this.setState({ error: err }));
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const { animal, breed, location, description, media, name } = this.state;
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
     );
+  }
 }
- 
+
 export default Details;
