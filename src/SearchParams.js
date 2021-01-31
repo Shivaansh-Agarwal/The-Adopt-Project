e811e25e66c078d495cdf3c4f569eac48cde3a59
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-01-31 14:54:15 +0530

    Async calls to Petfinder API, update Pet Component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  // UseEffect is disconnected from when the Render is happening.
+  // This schedules the following to run after the render happens
+  // It's not synchronous
+  // Why are we doing this?
+  // We want the user to see something immediately.
+  // and not wait for some AJAX call to bring back data
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedsStrings = apiBreeds.map(({ name }) => name);
+      //const breedStrings = apiBreeds.map((breedObj) => breedObj.name);
+      setBreeds(breedsStrings);
+    }, console.error);
+    // }, error => console.error(error));
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
