ccc29ba8bf1cbec9c3bec9e756b6dbf872a398b7
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-01-31 12:50:16 +0530

    Working with Hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import { render } from 'react-dom';
 // Now you can directly use render() instead of ReactDOM.render()
-import Pet from './Pet';
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 key="1">Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" key="2"/>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" key="3"/>
-      <Pet name="Doink" animal="Cat" breed="Mixed" key="4"/>
+      <SearchParams />
     </div>
   );
 };
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-    return (
-        <div>
-            <h1 key="1">{ name }</h1>
-            <h2 key="2">{ animal }</h2>
-            <h2 key="3">{ breed }</h2>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1 key="1">{name}</h1>
+      <h2 key="2">{animal}</h2>
+      <h2 key="3">{breed}</h2>
+    </div>
+  );
+}
